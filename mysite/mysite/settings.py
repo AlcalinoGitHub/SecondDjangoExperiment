24371fdb3c8896b76eb35702aa4f854290cda96d
AlcalinoGitHub <alcalinouwu69@gmail.com>
2023-02-19 01:02:42 -0600

    Added staticfiles directory
    python manage.py collecstatic

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&=fwo!ttte)bvav)yj#48y7c_rmaaj+c$%r!2f$#r!_w^==^@l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,16 +117,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
+import os
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, r'C:\Users\Yo\Desktop\MiPc\SecondDjangoExperiment\mysite\mysite\static'),
+    os.path.join(BASE_DIR, 'static')
 
 ]
