14200aed125396e64efbf1ef7cec0cb2b2d3c9be
AlcalinoGitHub <alcalinouwu69@gmail.com>
2023-02-18 21:28:11 -0600

    Added task creation functionality

--- mysite/Tasks/views.py
+++ mysite/Tasks/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from Tasks.models import Task
+from django.contrib import messages
 # Create your views here.
 
 def main(request):
-    return render(request, 'Main.html')
+    if request.method == 'POST':
+        Title = request.POST['Title']
+        Description = request.POST['Description']
+        Date = request.POST['DueDate']
+        try:
+            NewTask = Task.objects.create(Title = Title, Description = Description, DueDate = Date, Owner = request.user)
+            NewTask.save()
+            messages.info(request, f'Task {Title} has been created')
+            return redirect('/tasks/')
+        except:
+            messages.info(request, 'Fields cant be empty')
+            return redirect('/tasks/')
+    else:
+        Data = Task.objects.filter(Owner = request.user)
+        context = {'tareas': Data}
+        return render(request, 'Main.html', context)
